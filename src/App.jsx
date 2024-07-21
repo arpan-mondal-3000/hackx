45f8503e738f535844c325515e263bad97f04258
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-07-21 20:13:34 +0530

    firebase setup updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import "./App.css";
-import { app } from "./firebase";
 
 function App() {
   return (
     <>
-      <h1 className="bg-cyan-800 text-white">Coming Soon</h1>
+      <h1>Coming Soon</h1>
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { FirebaseProvider } from "./context/Firebase.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
+  </React.StrictMode>
+);

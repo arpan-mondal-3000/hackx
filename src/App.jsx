94fec8719607d6e2817d4358887854671e1fd58e
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-07-27 19:58:27 +0530

    used props in hackathon card

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,12 @@ import HackathonCard from "./components/HackathonCard.jsx";
 function App() {
   return (
     <>
-      <HackathonCard />
+      <HackathonCard
+        name="Hackathon Name"
+        imgSrc="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+        themes={["Web3", "Mobile", "Dev"]}
+        desc="This is the short description of the hackathon."
+      />
     </>
   );
 }
--- src/components/HackathonCard.jsx
+++ src/components/HackathonCard.jsx
@@ -3,34 +3,20 @@ function HackathonCard(props) {
     <>
       <div className="card w-80 font-sans border-2 border-white rounded-lg bg-white text-black">
         <div className="head">
-          <img
-            src="https://images.unsplash.com/photo-1515879218367-8466d910aaa4?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt=""
-            srcset=""
-            className="rounded-t-lg"
-          />
+          <img src={props.imgSrc} alt="" srcset="" className="rounded-t-lg" />
         </div>
         <div className="content p-4">
-          <div className="name text-2xl my-2">Hackathon name</div>
+          <div className="name text-2xl my-2">{props.name}</div>
           <div className="themes flex gap-2 my-2 flex-wrap">
-            <div className="border-2 border-gray-300 text-gray-500 rounded-3xl px-3 py-1">
-              Theme 1
-            </div>
-            <div className="border-2 border-gray-300 text-gray-500 rounded-3xl px-3 py-1">
-              Theme 2
-            </div>
-            <div className="border-2 border-gray-300 text-gray-500 rounded-3xl px-3 py-1">
-              Theme 3
-            </div>
+            {props.themes.map((element) => (
+              <div className="border-2 border-gray-300 text-gray-500 rounded-3xl px-3 py-1 hover:shadow-lg hover:cursor-pointer transition-all">
+                {element}
+              </div>
+            ))}
           </div>
-          <div className="desc">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati
-            facilis eum corrupti exercitationem fugit vero accusantium adipisci
-            aperiam unde et. Possimus distinctio id placeat deleniti suscipit,
-            aliquam magnam tenetur corrupti!
-          </div>
+          <div className="desc h-30 overflow-hidden">{props.desc}</div>
           <div className="btn my-2 flex justify-center align-middle">
-            <button className="border-2 border-green-500 px-5 py-2 rounded bg-green-500 text-white">
+            <button className="border-2 border-green-500 px-5 py-2 rounded bg-green-500 text-white hover:shadow-lg hover:shadow-green-400 transition-all">
               Apply Now
             </button>
           </div>

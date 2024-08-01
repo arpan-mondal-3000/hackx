e081345ddcbe55f0a145cf545ee04008c933d673
Sagnik Chakraborty <143476306+Sagnik-23@users.noreply.github.com>
2024-08-01 19:55:27 +0530

    scrollbar_corrected

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
-<<<<<<< HEAD
-=======
+
+
 import Signup from "./pages/signup/Signup.jsx";
 import HackathonCard from "./components/HackathonCard.jsx";
->>>>>>> b45b87058b01ad684b0a210ab6b44165afab8f81
 import LandingPage from "./pages/landingPage/landingPage.jsx";
 
 function App() {
   return (
     <>
-      <Login />
+      <LandingPage />
     </>
   );
 }

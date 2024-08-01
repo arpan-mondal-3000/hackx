1921bec65879b1a45346627bbb75c796174d475c
Sagnik Chakraborty <143476306+Sagnik-23@users.noreply.github.com>
2024-08-01 19:38:59 +0530

    Lol

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
-import HackathonCard from "./components/HackathonCard.jsx";
 import LandingPage from "./pages/landingPage/landingPage.jsx";
 
 function App() {

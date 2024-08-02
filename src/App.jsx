555d104ec49be4aed1db41d40cc91e9d9ec26c4f
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-08-02 10:38:30 +0530

    navbar text color change on hover

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Home from "./pages/home/Home.jsx";
 import Login from "./pages/login/Login.jsx";
 
-
 import Signup from "./pages/signup/Signup.jsx";
 import HackathonCard from "./components/HackathonCard.jsx";
 import LandingPage from "./pages/landingPage/landingPage.jsx";
@@ -9,7 +8,7 @@ import LandingPage from "./pages/landingPage/landingPage.jsx";
 function App() {
   return (
     <>
-      <Login />
+      <LandingPage />
     </>
   );
 }
